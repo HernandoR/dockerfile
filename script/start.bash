67f57b1b0a429d56f2a3a56ebb76af36a012297d
hujingnb <hujingnb@qq.com>
2022-03-13 17:44:59 +0800

    修改容器重启的 bug

--- script/start.bash
+++ script/start.bash
@@ -4,6 +4,7 @@
 chmod +x /opt/init.bash
 /opt/init.bash
 # 启动 4 个 worker 进程
+rm /run/fcgiwrap.socket # 防止容器上次强制停止, socket 仍然存在
 nohup fcgiwrap -f -c 4 -s unix:/run/fcgiwrap.socket </dev/null &>/dev/null &
 # 这里等待 socket 文件创建出来. 若立马执行 chown 命令, 文件可能不存在
 until ls /run/fcgiwrap.socket; do sleep 0.1; done
